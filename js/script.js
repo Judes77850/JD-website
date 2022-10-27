e4bb4280c5f52ac97b3a6d8dabb7d652b9512e9c
Julien Desaindes <julien.desaindes@gmail.com>
2022-10-27 18:20:24 +0200

    ajout premiere version barre menu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("phantomMenu");
+  if (window.scrollY >= 250) {
+    nav.style.position = "fixed";
+    nav.style.opacity = "1";
+    nav.style.marginTop = "0px";
+    nav.style.zIndex = "1";
+    nav.style.visibility = "visible";
+    nav.style.transitionDuration = "300ms";
+  } else {
+    nav.style.visibility = "hidden";
+    nav.style.transitionDuration = "0ms";
+  }
+});
