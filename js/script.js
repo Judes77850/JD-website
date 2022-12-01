c28a457e3599876a9ed7b8015e68430530e1cdaa
Julien Desaindes <juliendesaindes@MacBook-Air-de-Julien.local>
2022-12-01 17:26:08 +0100

    ajout nv fichiers

--- js/script.js
+++ js/script.js
@@ -1,53 +1,66 @@
 // showMenu(menu);
 
 window.addEventListener("scroll", () => {
-  const nav = document.querySelector("headerbis");
-  if (window.scrollY >= 100) {
+  const nav = document.getElementById("menuneonlogo");
+  const box = document.getElementById("neonlogobox");
+  if (window.scrollY >= 140) {
+    box.style.margin = "auto";
+    box.style.marginTop = "10px";
+    box.style.display = "flex";
+    box.style.flexDirection = "column";
+    box.style.justifyContent = "space-between";
+    box.style.alignItems = "center"
+    nav.style.borderRadius = "25px";
     nav.style.position = "fixed";
-    nav.style.height = "130px";
-    nav.style.opacity = "1";
-    nav.style.marginTop = "-20px";
+    nav.style.margin = "auto";
+    nav.style.display = "flex";
+    nav.style.justifyContent = "space-around";
+    nav.style.alignItems = "center";
     nav.style.zIndex = "1";
-    nav.style.visibility = "visible";
     nav.style.transitionDuration = "300ms";
     nav.style.backgroundColor = "black";
-  } else {
-    nav.style.visibility = "visible";
-    nav.style.transitionDuration = "0ms";
+  } else if (window.scrollY <= 140) {
+    nav.style.position = "relative";
+    nav.style.transitionDuration = "300ms";
+    nav.style.backgroundColor = "";
   }
 });
 
 document.getElementById("btn").addEventListener("click", function () {
   let nav = document.getElementById("bisMenu");
+  let btnClose = document.getElementById("btnClose");
+  let btnMenu = document.getElementById("btnMenu");
+  nav.style.animation = "bisMenuOpen"
+  nav.style.animationDuration = "500ms"
   nav.style.visibility = "visible";
   nav.style.height = "auto";
-  nav.style.marginBottom = "30px";
+  nav.style.padding = "0px"
+  nav.style.marginBottom = "10px";
   nav.style.marginTop = "20px";
   nav.style.fontSize = "1.5rem";
   nav.style.width = "90%";
+  btnClose.style.animation = "btnOpen";
+  btnClose.style.animationDuration = "400ms";
+  btnClose.style.animationDelais = "80ms";
+  btnMenu.style.animation = "btnMenuOpen";
+  btnMenu.style.animationDuration = "120ms";
+  btnMenu.style.animationDelay = "0ms";
 });
-
-// // document.getElementById("btn").addEventListener("click", function () {
-// //   let MenuBtn = document.getElementById("btn");
-// //   MenuBtn.style.visibility = "hidden";
-// // });
 
 document.getElementById("close").addEventListener("click", function () {
   let nav = document.getElementById("bisMenu");
+  let btnClose = document.getElementById("btnClose");
+  let btnMenu = document.getElementById("btnMenu");
+  nav.style.animation = "bisMenuClose"
+  nav.style.animationDuration = "500ms"
   nav.style.visibility = "hidden";
   nav.style.height = "0px";
   nav.style.width = "0px";
   nav.style.marginBottom = "0px";
+  nav.style.marginTop = "0px";
+  btnClose.style.animation = "btnClose";
+  btnClose.style.animationDuration = "500ms";
+  btnMenu.style.animation = "btnMenuClose";
+  btnMenu.style.animationDuration = "120ms";
+  btnMenu.style.animationDelay = "380ms";
 });
-
-// function showMenu(showMenu) {
-//   let btn = document.getElementById("btn");
-//   let close = document.getElementById("close");
-//   if (btn.addEventListener("click")) {
-//     nav.style.visibility = "visible";
-//   } else if (close.addEventListener("click")) {
-//     nav.style.visibility = "hidden";
-//   } else {
-//     console.log("no click");
-//   }
-// }
