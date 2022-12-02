67dcd0378a380df79b36a130ef33f8bb5fe288f7
Julien Desaindes <juliendesaindes@MacBook-Air-de-Julien.local>
2022-12-02 14:14:02 +0100

    ajout fonctions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("scroll", () => {
   const nav = document.getElementById("menuneonlogo");
+  let navBar = document.getElementById("bisMenu");
   const box = document.getElementById("neonlogobox");
   if (window.scrollY >= 140) {
     box.style.margin = "auto";
@@ -9,8 +10,8 @@ window.addEventListener("scroll", () => {
     box.style.display = "flex";
     box.style.flexDirection = "column";
     box.style.justifyContent = "space-between";
-    box.style.alignItems = "center"
-    nav.style.borderRadius = "25px";
+    box.style.alignItems = "center";
+    box.style.width = "100%";
     nav.style.position = "fixed";
     nav.style.margin = "auto";
     nav.style.display = "flex";
@@ -20,9 +21,10 @@ window.addEventListener("scroll", () => {
     nav.style.transitionDuration = "300ms";
     nav.style.backgroundColor = "black";
   } else if (window.scrollY <= 140) {
-    nav.style.position = "relative";
-    nav.style.transitionDuration = "300ms";
+    nav.style.position = "initial";
     nav.style.backgroundColor = "";
+    nav.style.marginTop = "0px";
+    navBar.style.position = "relative";
   }
 });
 
@@ -30,21 +32,22 @@ document.getElementById("btn").addEventListener("click", function () {
   let nav = document.getElementById("bisMenu");
   let btnClose = document.getElementById("btnClose");
   let btnMenu = document.getElementById("btnMenu");
-  nav.style.animation = "bisMenuOpen"
-  nav.style.animationDuration = "500ms"
+  nav.style.animation = "bisMenuOpen";
+  nav.style.animationDuration = "500ms";
   nav.style.visibility = "visible";
+  nav.style.position = "fixed";
   nav.style.height = "auto";
   nav.style.padding = "0px"
-  nav.style.marginBottom = "10px";
-  nav.style.marginTop = "20px";
+  nav.style.marginBottom = "0px";
+  nav.style.marginTop = "50px";
   nav.style.fontSize = "1.5rem";
   nav.style.width = "90%";
   btnClose.style.animation = "btnOpen";
-  btnClose.style.animationDuration = "400ms";
-  btnClose.style.animationDelais = "80ms";
+  btnClose.style.animationDuration = "500ms";
+  btnClose.style.animationDelais = "0ms";
   btnMenu.style.animation = "btnMenuOpen";
   btnMenu.style.animationDuration = "120ms";
-  btnMenu.style.animationDelay = "0ms";
+  btnMenu.style.animationDelay = "50ms";
 });
 
 document.getElementById("close").addEventListener("click", function () {
@@ -52,12 +55,13 @@ document.getElementById("close").addEventListener("click", function () {
   let btnClose = document.getElementById("btnClose");
   let btnMenu = document.getElementById("btnMenu");
   nav.style.animation = "bisMenuClose"
-  nav.style.animationDuration = "500ms"
+  nav.style.animationDuration = "500ms";
   nav.style.visibility = "hidden";
   nav.style.height = "0px";
   nav.style.width = "0px";
   nav.style.marginBottom = "0px";
   nav.style.marginTop = "0px";
+  nav.style.position = "fixed";
   btnClose.style.animation = "btnClose";
   btnClose.style.animationDuration = "500ms";
   btnMenu.style.animation = "btnMenuClose";
