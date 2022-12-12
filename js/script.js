91c73f680be41db79da580d3f12c2445b22e45aa
Julien Desaindes <juliendesaindes@MacBook-Air-de-Julien.local>
2022-12-12 11:01:54 +0100

    modif barre menu

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 window.addEventListener("scroll", () => {
-  const nav = document.getElementById("menuneonlogo");
-  let navBar = document.getElementById("bisMenu");
-  if (window.scrollY >= 128) {
+  const nav = document.getElementById("menuFixed");
+  const navBar = document.getElementById("bisMenu");
+  const main = document.getElementById("main");
+  if (window.scrollY >= 123) {
     nav.style.position = "fixed";
-    navBar.style.position = "initial";
-  } else if (window.scrollY <= 128) {
-    nav.style.position = "relative";
+    main.style.marginTop = "80px";
+  } else if (window.scrollY <= 123) {
+    nav.style.position = "";
     nav.style.marginTop = "0px";
+    navBar.style.position = "";
+    main.style.marginTop = "30px";
   } 
 });
+
+// window.addEventListener("scroll", () =>{
+// console.log(window.scrollY);
+// })
 
 document.getElementById("btn").addEventListener("click", function () {
   let nav = document.getElementById("bisMenu");
   let btnClose = document.getElementById("btnClose");
   let btnMenu = document.getElementById("btn");
+  let navBar = document.getElementById("bisMenu");
   nav.style.animation = "bisMenuOpen";
   nav.style.animationDuration = "500ms";
   nav.style.marginTop = "0px";
+  navBar.style.position = "relative";
   btnClose.style.animation = "btnMenuOpen";
   btnClose.style.animationDuration = "300ms";
   btnClose.style.zIndex = "2";
@@ -30,12 +39,14 @@ document.getElementById("btn").addEventListener("click", function () {
 
 document.getElementById("btnClose").addEventListener("click", function () {
   let nav = document.getElementById("bisMenu");
+  let navBar = document.getElementById("bisMenu");
   let btnMenu = document.getElementById("btn");
   let btnClose = document.getElementById("btnClose");
   nav.style.animation = "bisMenuClose";
-  nav.style.marginTop = "-50px";
+  nav.style.position = "relative";
   nav.style.zIndex = "1",
   nav.style.animationDuration = "500ms";
+  navBar.style.marginTop= "-50px";
   btnMenu.style.animation = "btnMenuOpen";
   btnMenu.style.animationDuration = "300ms";
   btnMenu.style.zIndex = "2";
