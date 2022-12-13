52c63cda2fe00bc6355cb9c8373bc2f734366fc4
Julien Desaindes <juliendesaindes@MacBook-Air-de-Julien.local>
2022-12-13 14:29:55 +0100

    design

--- js/script.js
+++ js/script.js
@@ -26,15 +26,8 @@ document.getElementById("btn").addEventListener("click", function () {
   nav.style.animationDuration = "500ms";
   nav.style.marginTop = "0px";
   navBar.style.position = "relative";
-  btnClose.style.animation = "btnMenuOpen";
-  btnClose.style.animationDuration = "300ms";
-  btnClose.style.zIndex = "2";
-  btnClose.style.opacity = "1";
-  btnClose.style.color = "$color-2";
   btnMenu.style.animation = "btnMenuOpen";
   btnMenu.style.animationDuration = "300ms";
-  btnMenu.style.zIndex = "1";
-  btnMenu.style.opacity = "0";
 });
 
 document.getElementById("btnClose").addEventListener("click", function () {
@@ -47,15 +40,6 @@ document.getElementById("btnClose").addEventListener("click", function () {
   nav.style.zIndex = "1",
   nav.style.animationDuration = "500ms";
   navBar.style.marginTop= "-50px";
-  btnMenu.style.animation = "btnMenuOpen";
-  btnMenu.style.animationDuration = "300ms";
-  btnMenu.style.zIndex = "2";
-  btnMenu.style.opacity = "1";
-  btnClose.style.animation = "btnMenuOpen";
-  btnClose.style.animationDuration = "300ms";
-  btnClose.style.zIndex = "1";
-  btnClose.style.opacity = "0";
-  btnClose.style.color = "black";
 });
 
 // document.querySelector("fa-plus").addEventListener("click", function () {
